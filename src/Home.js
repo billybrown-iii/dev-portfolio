8e13a54239576ab7282d646f006f07f8f04c0de0
BillyBDev <BillyBDev@gmail.com>
2021-03-27 18:50:00 -0700

    Added content, styling

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,11 @@ import React from "react"
 function Home(props) {
     return (
         <div>
-            <h1 className="section-title home">About</h1>
-            <div className="card home-card">
+            <div className="card">
                 <div className="card-content">
-                    Hi
+                    <div className="card-title">About</div>
+                    <div className="content">I am a web developer who started programming by writing scripts in AutoHotKey in order to automate portions of my office work.  I soon became addicted to solving problems and building solutions with code.</div>
+                    <div className="content">Today, my primary focus is on clean layouts that provide a graceful and effortless user experience.  I am excited to continue building out my portfolio and begin seeking a full-time developer position.</div>
                 </div>
             </div>
         </div>
